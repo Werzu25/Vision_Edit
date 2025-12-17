0b3cb62e5d5d2525a1b0e6c27cb88dc6b42823d0
Leon Wohlgemuth <lwohlgemuth@tsn.at>
2025-12-17 16:50:56 +0100

    Add user model properties and complete registration logic in `RegistrationViewModel`

--- Vision Edit/ViewModels/RegistrationViewModel.cs
+++ Vision Edit/ViewModels/RegistrationViewModel.cs
@@ -1,11 +1,19 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Net.Http.Json;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Models;
 
 namespace Vision_Edit.ViewModels;
 
 public partial class RegistrationViewModel : ObservableObject
 {
     private readonly HttpClient _httpClient;
+
+    [ObservableProperty] private string _username;
+    [ObservableProperty] private string _firstName;
+    [ObservableProperty] private string _lastName;
+    [ObservableProperty] private string _email;
+    [ObservableProperty] private string _password;
     
     public RegistrationViewModel (HttpClient httpClient)
     {
@@ -21,6 +29,18 @@ public partial class RegistrationViewModel : ObservableObject
     [RelayCommand]
     public async Task Register()
     {
-        _httpClient.PostAsync("", new StringContent(""));
+        UserModel user = new()
+        {
+            Username = _username,
+            FirstName = _firstName,
+            LastName = _lastName,
+            Email = _email,
+            Password = _password
+        }; 
+        var result =  await _httpClient.PostAsJsonAsync("/User", user);
+        if (result.IsSuccessStatusCode)
+        {
+            SwitchToMainPage();
+        }
     }
 }
