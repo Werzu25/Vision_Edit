ce1c113a790a52dcaf5f0c1ac64529ece0daeb76
Leon Wohlgemuth <lwohlgemuth@tsn.at>
2025-12-17 15:11:09 +0100

    Add `RegistrationPage` view with initialization logic

---
+++ Vision Edit/Views/RegistrationPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vision_Edit.Views;
+
+public partial class RegistrationPage : ContentPage
+{
+    public RegistrationPage()
+    {
+        InitializeComponent();
+    }
+}
