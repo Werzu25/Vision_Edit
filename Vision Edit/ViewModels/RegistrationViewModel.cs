384470f21da02f40f7ea08bbdf22d239afb08fe8
Leon Wohlgemuth <lwohlgemuth@tsn.at>
2025-12-17 15:11:02 +0100

    Add `RegistrationViewModel` with registration and navigation logic

---
+++ Vision Edit/ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Vision_Edit.ViewModels;
+
+public partial class RegistrationViewModel : ObservableObject
+{
+    private readonly HttpClient _httpClient;
+    
+    public RegistrationViewModel (HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    
+    [RelayCommand]
+    public async Task SwitchToMainPage()
+    {
+        await Shell.Current.GoToAsync("//MainPage");
+    }
+
+    [RelayCommand]
+    public async Task Register()
+    {
+        _httpClient.PostAsync("", new StringContent(""));
+    }
+}
