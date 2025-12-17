420e17a5666554578b1d4b558676426abe6246c0
Leon Wohlgemuth <lwohlgemuth@tsn.at>
2025-12-17 16:50:47 +0100

    Inject `RegistrationViewModel` into `RegistrationPage` and set as BindingContext

--- Vision Edit/Views/RegistrationPage.xaml.cs
+++ Vision Edit/Views/RegistrationPage.xaml.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vision_Edit.ViewModels;
 
 namespace Vision_Edit.Views;
 
 public partial class RegistrationPage : ContentPage
 {
-    public RegistrationPage()
+    public RegistrationPage(RegistrationViewModel viewModel)
     {
+        BindingContext = viewModel;
         InitializeComponent();
     }
 }
