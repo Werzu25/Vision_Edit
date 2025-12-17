b6e2fe7949151abab6f286e335c3a5909d540400
Leon Wohlgemuth <lwohlgemuth@tsn.at>
2025-12-17 14:33:09 +0100

    Rename `User` class to `UserModel` and add properties with EF Core annotations

---
+++ Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Models;
+
+[Index(nameof(Username), IsUnique = true)]
+public class UserModel
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
