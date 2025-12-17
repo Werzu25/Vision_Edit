13aacad8fd3dfaa1932f956e75801b88ad7fc14d
Leon Wohlgemuth <lwohlgemuth@tsn.at>
2025-12-17 14:31:00 +0100

    Add Entity Framework DbContext to DbManager with MySQL configuration

--- ORM/Services/DbManager.cs
+++ ORM/Services/DbManager.cs
@@ -1,6 +1,14 @@
-﻿namespace ORM.Services;
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
 
-public class DbManager
+namespace ORM.Services;
+
+public class DbManager : DbContext
 {
-    
+    public DbSet<UserModel> Users { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string connectionString = "Server=localhost;Database=vision_edit;User=root;Password=root;";
+        optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    }
 }
