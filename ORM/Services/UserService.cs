09b964680a5e9e6414d01770e9cdf6f8ce5a835b
Leon Wohlgemuth <lwohlgemuth@tsn.at>
2025-12-17 14:32:42 +0100

    Add User controller with CRUD endpoints

---
+++ ORM/Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace ORM.Services;
+
+public class UserService
+{
+    private readonly DbManager _dbManager;
+    
+    public UserService(DbManager dbManager)
+    {
+        _dbManager = dbManager;
+    }
+    
+    public async Task<List<UserModel>> GetAllUsers()
+    {
+        return await _dbManager.Users.ToListAsync();
+    }
+
+    public async Task<UserModel> GetUserById(int userId)
+    {
+        return await _dbManager.Users.FindAsync(userId);
+    }
+
+    public async Task<UserModel> CreateUser(UserModel userModel)
+    {
+        PasswordHasher<UserModel> passwordHasher = new PasswordHasher<UserModel>();
+        passwordHasher.HashPassword(userModel, userModel.Password);
+        return (await _dbManager.AddAsync(userModel)).Entity;
+    }
+}
---
+++ Vision Edit API/Controllers/User.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using ORM.Services;
+
+namespace Vision_Edit_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class User : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public User(DbManager dbManager)
+        {
+            _userService = new UserService(dbManager); 
+        }
+        
+        // GET: api/<User>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            List<UserModel> allUsers =  await _userService.GetAllUsers();
+            if(allUsers.Count == 0)
+                return NoContent();
+            return Ok(allUsers);
+        }
+
+        // GET api/<User>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<User>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] UserModel user)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var response = await _userService.CreateUser(user);
+            if (user == null)
+                return NoContent();
+            return Created(new Uri($"{Request.Scheme}://{Request.Host}{Request.Path}/{response.Id}"), response);
+        }
+
+        // PUT api/<User>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<User>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
