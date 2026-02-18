b7a087ec09d9cedeb504156671578babc29161b4
Leon Wohlgemuth <leonw@astecs.com>
2026-02-18 15:29:01 +0100

    Add EditorPage, EditorView, and GlowBorderView components
    Introduced EditorPage as a container for the new EditorView, which features a bordered editing area. Added GlowBorderView as a placeholder for future UI enhancements. All components include initial XAML and code-behind stubs.

---
+++ Vision Edit/Views/EditorPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vision_Edit.Views;
+
+public partial class EditorPage : ContentPage
+{
+    public EditorPage()
+    {
+        InitializeComponent();
+    }
+}
