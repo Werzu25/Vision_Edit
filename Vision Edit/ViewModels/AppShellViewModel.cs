927bd01a87d4d3335d7051ae1dfc4c8be4723a95
Leon Wohlgemuth <leonw@astecs.com>
2026-02-18 15:28:03 +0100

    Refactor AppShellViewModel UserManager handling
    Changed UserManager to an observable property, removed IsLoggedIn property and related event handling, and updated Logout command to use the new UserManager property. This simplifies state management and improves property accessibility.

--- Vision Edit/ViewModels/AppShellViewModel.cs
+++ Vision Edit/ViewModels/AppShellViewModel.cs
@@ -6,28 +6,17 @@ namespace Vision_Edit.ViewModels;
 
 public partial class AppShellViewModel : ObservableObject
 {
-    private readonly UserManager _userManager;
-
-    [ObservableProperty] private bool _isLoggedIn; 
+    [ObservableProperty]
+    private UserManager _userManager;
 
     public AppShellViewModel(UserManager userManager)
     {
         _userManager = userManager;
-        _userManager.PropertyChanged += OnUserManagerPropertyChanged;
-        IsLoggedIn = _userManager.IsLoggedIn;
-    }
-
-    private void OnUserManagerPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-    {
-        if (e.PropertyName == nameof(UserManager.IsLoggedIn))
-        {
-            IsLoggedIn = _userManager.IsLoggedIn;
-        }
     }
 
     [RelayCommand]
     private void Logout()
     {
-        _userManager.Username = string.Empty;
+        UserManager.Username = string.Empty;
     }
 }
